f8b9e875d43157e1d0447e146ea69d5125135f51
Rodrigo Guedes da Silva <79825051+RodrigoGuedesdaSilva@users.noreply.github.com>
2021-10-04 08:36:53 -0300

    app.js Update

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
-const menu = document.querySelector('.hamb-menu');
-const menuLink = document.querySelector('.menu');
+const menu = document.getElementById('hamb-menu');
+const line = document.querySelector('.line')
+const menuLink = document.getElementById('menu');
 const menuBoard = document.querySelector('.invisible-board');
 const glassEffectH1 = document.querySelector('h1'); 
 const glassEffectP = document.querySelector('p'); 
@@ -7,12 +8,26 @@ const glassEffectBtn = document.querySelector('.style-btn');
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('active');
+    line.classList.toggle('active');
     menuLink.classList.toggle('is-active');
     menuBoard.classList.toggle('activated'); 
     glassEffectH1.classList.toggle('glass-effect'); 
     glassEffectP.classList.toggle('glass-effect'); 
     glassEffectBtn.classList.toggle('glass-effect'); 
 })
+
+document.onclick = function(e) {
+    if(e.target.id !== 'hamb-menu' && e.target.id !== 'menu') {
+        menu.classList.remove('active');
+        line.classList.remove('active');
+        menuLink.classList.remove('is-active');
+        menuBoard.classList.remove('activated');
+        glassEffectH1.classList.remove('glass-effect'); 
+        glassEffectP.classList.remove('glass-effect'); 
+        glassEffectBtn.classList.remove('glass-effect'); 
+    }
+}
+
 
 // init Isotope
 var $grid = $('#portfolio .container .row').isotope({
