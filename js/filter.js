b283ddf415dba4c8425d2cc7fd4487445ed95c2a
Rodrigo Guedes da Silva <79825051+RodrigoGuedesdaSilva@users.noreply.github.com>
2021-10-04 22:35:56 -0300

    Added filter.js file

---
+++ js/filter.js
@@ -0,0 +1,19 @@
+
+// init Isotope
+var $grid = $('#portfolio .container .row').isotope({
+    // options
+  });
+  // filter items on button click
+  $('.filter-button-group').on( 'click', 'button', function() {
+    var filterValue = $(this).attr('data-filter');
+    resetFilterBtns();
+    $(this).addClass('active-filter-btn');
+    $grid.isotope({ filter: filterValue });
+  });
+
+  var filterBtns = $('.filter-button-group').find('button');
+  function resetFilterBtns() {
+      filterBtns.each(function() {
+          $(this).removeClass('active-filter-btn');
+      });
+  }
