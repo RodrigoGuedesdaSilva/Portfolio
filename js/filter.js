a5cbae74d3dcc5e81ab2a7b4b01f5241b435cf21
Rodrigo Guedes da Silva <rodrigogsilva20@outlook.com>
2021-10-11 11:37:50 -0300

    Update

--- js/filter.js
+++
@@ -1,9 +0,0 @@
-  // init Isotope
-  var $grid = $('.project-list').isotope({
-    // options
-  });
-  // filter items on button click
-  $('.filter-button-group').on( 'click', 'button', function() {
-    var filterValue = $(this).attr('data-filter');
-    $grid.isotope({ filter: filterValue });
-  });
